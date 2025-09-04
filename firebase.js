ee2a5e43d95917f3131f89ff0c9e1d5811167373
kumarvivek93296-alt <kumarvivek93296@gmail.com>
2025-09-04 13:32:28 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,14 @@
+
+// Firebase Config (apna config paste karna yaha)
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+// Init Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
